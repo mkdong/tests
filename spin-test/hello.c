b47e28c0271e341299a1bade222c9d8a95d18a1e
Mingkai Dong <mingkaidong@gmail.com>
2016-11-22 22:33:29 +0800

    feat(spin): Add atomic test

--- spin-test/hello.c
+++ spin-test/hello.c
@@ -8,43 +8,76 @@ MODULE_AUTHOR("MK");
 MODULE_DESCRIPTION("Linux kernel modules code-frames.");
 
 struct res {
+	struct llist_node lnode;
 	struct list_head node;
 	void *ptr;
 };
 
 spinlock_t lock;
 struct list_head pool;
+struct llist_head lpool;
 
-void *alloc(bool uselock)
-{
-	struct res *res;
-	void *r;
-	if (uselock) spin_lock(&lock);
-	if (list_empty(&pool)) {
-		if (uselock) spin_unlock(&lock);
-		return NULL;
-	}
-	res = list_first_entry(&pool, struct res, node);
-	list_del(&res->node);
-
-	if (uselock) spin_unlock(&lock);
-	r = res->ptr;
-	kfree(res);
-	return r;
+#define DEF_ALLOC(name, pre, post, empty, first, del) \
+void *alloc_##name(void) \
+{ \
+	struct res *res; \
+	void *r; \
+	pre; \
+	if (empty) { \
+		post; \
+		return NULL; \
+	} \
+	res = first; \
+	del; \
+	post; \
+	r = res->ptr; \
+	kfree(res); \
+	return r; \
 }
 
-void free(void *a, bool uselock)
-{
-	struct res *res;
-	if (a == NULL) return;
-	if (uselock) spin_lock(&lock);
-	res = kmalloc(sizeof(struct res), GFP_KERNEL);
-	res->ptr = a;
-	list_add(&res->node, &pool);
-	if (uselock) spin_unlock(&lock);
+DEF_ALLOC(vanilla, , ,
+	list_empty(&pool),
+	list_first_entry(&pool, struct res, node),
+	list_del(&res->node))
+
+DEF_ALLOC(spin,
+	spin_lock(&lock),
+	spin_unlock(&lock),
+	list_empty(&pool),
+	list_first_entry(&pool, struct res, node),
+	list_del(&res->node))
+
+DEF_ALLOC(lockfree, , ,
+	llist_empty(&lpool),
+	llist_entry(llist_del_first(&lpool), struct res, lnode),
+	)
+
+
+
+#define DEF_FREE(name, pre, post, add) \
+void free_##name(void *a) \
+{\
+	struct res *res; \
+	if (a == NULL) return; \
+	pre; \
+	res = kmalloc(sizeof(struct res), GFP_KERNEL); \
+	res->ptr = a; \
+	add; \
+	post; \
 }
 
-void test(bool uselock)
+DEF_FREE(vanilla, , ,
+	list_add(&res->node, &pool))
+
+DEF_FREE(spin,
+	spin_lock(&lock),
+	spin_unlock(&lock),
+	list_add(&res->node, &pool))
+
+DEF_FREE(lockfree, , ,
+	llist_add(&res->lnode, &lpool))
+
+void test(void *(*_alloc)(void), void (*_free)(void *), const char *prompt)
 {
 	struct timespec start;
 	struct timespec end;
@@ -56,37 +89,43 @@ void test(bool uselock)
 	spin_lock_init(&lock);
 	INIT_LIST_HEAD(&pool);
 	for (i = 0; i < N; ++i) {
-		free((void *)i + 1, uselock);
+		_free((void *)i + 1);
 	}
 	for (i = 0; i < N; ++i) {
 		getrawmonotonic(&start);
-		alloc(uselock);
+		_alloc();
 		getrawmonotonic(&end);
 		alloc_nsec += (end.tv_sec - start.tv_sec) * (u64)1e9
 			+ (end.tv_nsec - start.tv_nsec);
 	}
 	for (i = 0; i < N; ++i) {
 		getrawmonotonic(&start);
-		free((void *)i + 1, uselock);
+		_free((void *)i + 1);
 		getrawmonotonic(&end);
 		free_nsec += (end.tv_sec - start.tv_sec) * (u64)1e9
 			+ (end.tv_nsec - start.tv_nsec);
 	}
-	pr_info("uselock=%d alloc %d times costs nsec: %llu\n",
-		uselock, N, alloc_nsec);
-	pr_info("uselock=%d free  %d times costs nsec: %llu\n",
-		uselock, N, free_nsec);
+	pr_info("%s alloc %d times costs nsec: %llu\n",
+		prompt, N, alloc_nsec);
+	pr_info("%s free  %d times costs nsec: %llu\n",
+		prompt, N, free_nsec);
 }
 
 int __init hello_init(void)
 {
 	pr_info("Hello World!\n");
-	test(true);
-	test(false);
-	test(true);
-	test(false);
-	test(true);
-	test(false);
+	test(alloc_vanilla, free_vanilla, "vanilla");
+	test(alloc_vanilla, free_vanilla, "vanilla");
+	test(alloc_vanilla, free_vanilla, "vanilla");
+	test(alloc_vanilla, free_vanilla, "vanilla");
+	test(alloc_spin, free_spin, "spinlock");
+	test(alloc_spin, free_spin, "spinlock");
+	test(alloc_spin, free_spin, "spinlock");
+	test(alloc_spin, free_spin, "spinlock");
+	test(alloc_lockfree, free_lockfree, "lockfree");
+	test(alloc_lockfree, free_lockfree, "lockfree");
+	test(alloc_lockfree, free_lockfree, "lockfree");
+	test(alloc_lockfree, free_lockfree, "lockfree");
 	return 0;
 }
 
